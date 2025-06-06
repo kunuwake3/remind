16eddcb6437de9d8e9af248061073f9a320caba7
kunuwake3 <kunuwake3@gmail.com>
2025-06-07 01:58:23 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,34 @@
 #include <QTimer>
 #include "main_m1.h"
 #include <QMessageBox>
+#include <QPalette>
+#include <QStyleFactory>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    // Включить стиль Fusion (одинаково на всех платформах)
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
+
+    // Создать тёмную палитру
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(25,25,25)); // подложка у QLineEdit, QTextEdit
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    a.setPalette(darkPalette);
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
